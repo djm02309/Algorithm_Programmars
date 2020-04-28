3ede62e7538b4627d2a901c79370126db388f55a
djm02309 <djm02309@naver.com>
2020-04-28 18:15:28 +0900

    solved new problem(https://programmers.co.kr/learn/courses/30/lessons/42748)

---
+++ src/sort/numberK.java
@@ -0,0 +1,36 @@
+package sort;
+
+import java.net.Inet4Address;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class numberK {
+    public int[] solution(int[] array, int[][] commands) {
+        ArrayList<Integer> ans = new ArrayList();
+        for(int i = 0; i < commands.length; i++){
+            int length = commands[i][1] - commands[i][0] +1; //자른 배열의 길이
+            int[] newArray =  new int[length];
+            for(int j = commands[i][0]-1, k = 0; j<= commands[i][1]-1 && k <newArray.length ; j++, k++){
+                newArray[k] = array[j];
+            }
+            Arrays.sort(newArray);
+            ans.add(newArray[commands[i][2]-1]);
+        }
+        int[] answer = new int[ans.size()];
+        for(int i=0 ; i< ans.size(); i++){
+            answer[i] = ans.get(i);
+        }
+        return answer;
+    }
+    public static void main(String[] args){
+        numberK k =new numberK();
+        int[] answer = {};
+        int[] array = {1,5,2,6,3,7,4};
+        int[][] commands = {{2,5,3},{4,4,1},{1,7,3}};
+//        int[][] commands = {{1,7,3}};
+        answer = k.solution(array, commands);
+        for(int i=0 ; i< answer.length; i++){
+            System.out.print(answer[i]);
+        }
+    }
+}
