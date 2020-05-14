9f5c55bdc13c00e8b17df09814f071ca7d0a695c
Sooyeon <djm02309@naver.com>
2020-05-14 23:51:38 +0900

    solved part of new problem(https://programmers.co.kr/learn/courses/30/lessons/42839)

--- src/fullSearch/FindSosu.java
+++ src/fullSearch/FindSosu.java
@@ -8,4 +8,23 @@ public class FindSosu {
         int answer = 0;
         return answer;
     }
+
+    public int[] isPrime (int n) {
+        for (int i = 2; i <= n; i++) {
+            boolean isPrime = true;
+            // 초기 값
+            // for(int j=2; j<=i/2; j++) {
+            for (int j = 2; j * j <= i; j++) {
+                if (i % j == 0) {
+                    // 소수가 아니다.
+                    isPrime = false;
+                    break;
+                }
+            }
+            if (isPrime) {
+                // 소수 일때만 값을 출력
+                System.out.println(i + " ");
+            }
+        }
+    }
 }
