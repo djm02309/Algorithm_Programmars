64a1e5a54d3de6796a3395030e601a2fd56303f9
Sooyeon <djm02309@naver.com>
2020-12-06 22:45:38 +0900

    changed package

---
+++ src/ETC/CheckBracket.java
@@ -0,0 +1,22 @@
+package ETC;
+//https://programmers.co.kr/learn/courses/30/lessons/60058
+
+public class CheckBracket {
+    public String solution(String p) {
+        String answer = "";
+        return "";
+    }
+    String makeCorrect(String w){
+        if(w.isBlank()){
+            return "";
+        }
+        else{
+            int divid = division(w);
+            return "";
+        }
+    }
+    ///균형잡인 문자열로 나누기
+    int division(String w){
+        return 0;
+    }
+}
