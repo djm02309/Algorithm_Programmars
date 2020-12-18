a2cfe8b8d8c8bf92a03a587481d1e43b5e4c4a2a
Sooyeon <djm02309@naver.com>
2020-12-18 22:45:53 +0900

    trying problem(https://programmers.co.kr/learn/courses/30/lessons/60059)

---
+++ src/Kakao2020/KeyAndLock.java
@@ -0,0 +1,13 @@
+package Kakao2020;
+//출처: https://programmers.co.kr/learn/courses/30/lessons/60059
+public class KeyAndLock {
+    public boolean solution(int[][] key, int[][] lock) {
+        //키 배열 회전, 키 배열 이동
+        //회전?
+
+        //이동?
+        boolean answer = true;
+
+        return answer;
+    }
+}
